e83cc770bfd1446bdc6c142272ae6ec2891923d3
Thomas Dufourt <tj.dufourt@gmail.com>
2023-03-09 16:31:39 +0100

    confetti when click head

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-// confetti({
-//   particleCount: 100,
-//   spread: 50,
-//   origin: { y: 0.5, x: 0 },
-//   angle: 45
-// });
+const tomHead = document.querySelector("section.hero img");
 
-// confetti({
-//   particleCount: 100,
-//   spread: 50,
-//   origin: { y: 0.5, x: 1 },
-//   angle: 135
-// });
+tomHead.addEventListener("click", () => {
+confetti({
+    particleCount: 100,
+    spread: 50,
+    origin: { y: 1, x: 0.5 },
+    angle: 90,
+  });
+
+
+  // confetti({
+  //   particleCount: 100,
+  //   spread: 50,
+  //   origin: { y: 0.5, x: 0 },
+  //   angle: 45,
+  // });
+
+  // confetti({
+  //   particleCount: 100,
+  //   spread: 50,
+  //   origin: { y: 0.5, x: 1 },
+  //   angle: 135,
+  // });
+});
 
 const lines = document.querySelectorAll("section.hero div.container div span");
 
@@ -24,7 +36,7 @@ function linesAnitmation(lines) {
       line.style.transitionDelay = `${second}ms`;
       line.classList.toggle("bounce");
     });
-    time = lines[0].classList.contains("bounce") ? 6000 : 2000; //some new value
+    time = lines[0].classList.contains("bounce") ? 6000 : 2000;
     intId = window.clearInterval(intId);
     linesAnitmation(lines);
   }, time);
@@ -36,15 +48,12 @@ setInterval(function () {
   bubbles.forEach((bubble) => {
     const second = Math.random() * (1500 - 0) + 0;
     const scale = Math.random() * (1.2 - 0.9) + 0.9;
-    const x = Math.random() * (15 - (-15)) + (-15);
-    const y = Math.random() * (15 - (-15)) + (-15);
+    const x = Math.random() * (15 - -15) + -15;
+    const y = Math.random() * (15 - -15) + -15;
     bubble.style.transitionDelay = `${second}ms`;
-    bubble.style.scale = scale
-    bubble.style.transform = `translate(${x}%, ${y}%)`
+    bubble.style.scale = scale;
+    bubble.style.transform = `translate(${x}%, ${y}%)`;
   });
 }, 3000);
 
-
 linesAnitmation(lines);
-
-
