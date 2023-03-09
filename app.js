92d3325cc6255c0b55c6d16a6e2441ebd0e5d905
Thomas Dufourt <tj.dufourt@gmail.com>
2023-03-09 11:28:44 +0100

    random lines appear

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 
 
-confetti({
-  particleCount: 100,
-  spread: 50,
-  origin: { y: 0.5, x: 0 },
-  angle: 45
-});
+// confetti({
+//   particleCount: 100,
+//   spread: 50,
+//   origin: { y: 0.5, x: 0 },
+//   angle: 45
+// });
 
-confetti({
-  particleCount: 100,
-  spread: 50,
-  origin: { y: 0.5, x: 1 },
-  angle: 135
-});
+// confetti({
+//   particleCount: 100,
+//   spread: 50,
+//   origin: { y: 0.5, x: 1 },
+//   angle: 135
+// });
+
+const lines = document.querySelectorAll('section.hero div.container div span')
+
+console.log(lines)
+
+lines.forEach(line => {
+  const second = Math.random() * (1500 - 0) + 0;
+  line.style.transitionDelay = `${second}ms`
+  line.classList.add('bounce')
+})
