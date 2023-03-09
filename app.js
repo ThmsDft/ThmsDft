e6957aa5dbf7d163494bdffce5606d82110d89b9
Thomas Dufourt <tj.dufourt@gmail.com>
2023-03-09 11:43:34 +0100

    repeat animation lines

--- app.js
+++ app.js
@@ -14,10 +14,12 @@
 
 const lines = document.querySelectorAll("section.hero div.container div span");
 
-window.addEventListener("load", () => {
+setInterval(function () {
   lines.forEach((line) => {
     const second = Math.random() * (1500 - 0) + 0;
     line.style.transitionDelay = `${second}ms`;
-    line.classList.add("bounce");
-  });
-});
+    line.classList.toggle("bounce");
+    console.log('test')
+  })
+}, 2500);
+
