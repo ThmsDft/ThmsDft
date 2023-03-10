9e40dc8444330662aa2d4bb0871acbc492aa9f59
Thomas Dufourt <tj.dufourt@gmail.com>
2023-03-10 11:11:27 +0100

    messages animation reveal

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const tomHead = document.querySelector("section.hero img");
 
 tomHead.addEventListener("click", () => {
-confetti({
+  confetti({
     particleCount: 100,
     spread: 50,
     origin: { y: 1, x: 0.5 },
     angle: 90,
   });
-
 
   // confetti({
   //   particleCount: 100,
@@ -57,3 +56,45 @@ setInterval(function () {
 }, 3000);
 
 linesAnitmation(lines);
+
+/* =========SCROLL REVEAL========= */
+
+const leftMessages = document.querySelectorAll(
+  "section.about div:nth-child(odd)"
+);
+const rigthMessages = document.querySelectorAll(
+  "section.about div:nth-child(even)"
+);
+
+for (const leftMessage of leftMessages) {
+  leftMessage.classList.add("left");
+}
+
+const messages = [...leftMessages, ...rigthMessages];
+
+function scrollReveal() {
+  const revealPoint = 10;
+
+  for (const message of messages) {
+    var windowHeight = window.innerHeight;
+    var revealTop = message.getBoundingClientRect().top;
+    const messageClass = message.classList.contains('left') ? 'slide-right' : 'slide-left'
+    if (revealTop < windowHeight - revealPoint) {
+      message.classList.add(messageClass);
+    } else {
+      message.classList.remove(messageClass);
+    }
+  }
+
+  // for (var i = 0; i < revealElements.length; i++) {
+  //   var windowHeight = window.innerHeight;
+  //   var revealTop = revealElements[i].getBoundingClientRect().top;
+  //   if (revealTop < windowHeight - revealPoint) {
+  //     revealElement[i].classList.add("slide-right");
+  //   } else {
+  //     revealElement[i].classList.remove("slide-right");
+  //   }
+  // }
+}
+
+window.addEventListener("scroll", scrollReveal);
