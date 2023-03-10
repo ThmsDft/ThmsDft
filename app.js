5fefd57c505e0977d9cb9b883830c49755fe0521
Thomas Dufourt <tj.dufourt@gmail.com>
2023-03-10 16:36:43 +0100

    blur background

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('.hamburger')
+const header = document.querySelector('header')
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle('active')
+  header.classList.toggle('active')
 });
 
 const tomHead = document.querySelector("section.hero img");
